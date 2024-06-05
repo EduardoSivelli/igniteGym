ef514f76a20ebd217cd4c3b6e58e066ca37da73f
EduardoSivelli <marquessivelli@hotmail.com>
2024-06-05 20:14:31 -0300

    fixing bugs and finishing screens

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,6 +2,7 @@ import { HStack, VStack, Heading, Text, Icon } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
 
 export function HomeHeader(){
   return(
@@ -13,23 +14,24 @@ export function HomeHeader(){
         mr={4}
       />
 
-      <VStack>
+      <VStack flex={1}>
         <Text color='gray.100' fontSize='md'>
           Olá,
         </Text>
 
         <Heading color='gray.100' fontSize='md'>
-          Eduardo
+          Eduardo Marques Sivelli 
         </Heading>
       </VStack>
 
-      <Icon 
-        as={MaterialIcons}
-        name="logout"
-        color="gray.200"
-        size={7}
-      />
-      
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons}
+          name="logout"
+          color="gray.200"
+          size={7}
+        />
+      </TouchableOpacity>
     </HStack>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,71 @@
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { HStack, VStack, FlatList, Heading, Text } from "native-base"; 
+
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+
+import { ExerciseCard } from "@components/ExerciseCard";
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, Text, VStack } from "native-base"; 
 
 export function Home(){
+  const [groupSelected, setGroupSelected] = useState('costas')
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [exercises, setExercises] = useState(['Puxada Frontal','Remada Curvada', 'Remada Unilateral', 'Levantamento terra'])
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails(){
+    navigation.navigate('exercise')
+  }
+
   return(
     <VStack flex={1}>
-      <HomeHeader 
+      <HomeHeader />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group 
+          name={item}
+          isActive={(groupSelected).toLocaleUpperCase() === (item).toLocaleUpperCase()}
+          onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator = {false}
+        _contentContainerStyle={{ px: 8}}
+        my={10}
+        maxH={10}
+        minH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard 
+              onPress={handleOpenExerciseDetails}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20}}
+        />
+
+      </VStack>
     </VStack>
   )
 }
